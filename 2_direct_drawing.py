6841a22b3a2a20ee68dea9bd887fcffcf122f6a7
Hiç Farketmez <hicfarketmezfarkeder@gmail.com>
2025-06-27 14:29:14 +0300

    click for circle

---
+++ 2_direct_drawing.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(image, (x, y), 50, (0, 255, 0), thickness=-1)
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        cv2.circle(image, (x, y), 50, (0, 0, 255), thickness=5)
+
+cv2.namedWindow("Image")
+
+cv2.setMouseCallback("Image", draw_circle)
+
+
+image = np.zeros((512, 512, 3), dtype=np.int8)
+while True:
+    cv2.imshow("Image", image)
+    key = cv2.waitKey(20) & 0xFF == 27  # Escape key to exit
+    if key:
+        break
+cv2.destroyAllWindows()
+
